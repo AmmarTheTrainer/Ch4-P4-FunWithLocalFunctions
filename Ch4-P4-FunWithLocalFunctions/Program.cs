5caff1054992e9d45cb4bad4299b5d2e38ba387b
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-14 01:17:19 +0500

    first commit from LEC

---
+++ Ch4-P4-FunWithLocalFunctions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_P4_FunWithLocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lecture 4 - Project 4 - Local Functions ");
+
+
+
+            Console.ReadLine();
+        }
+
+        static int AddWrapper(int x, int y)
+        {
+            //Do some validation here
+            return Add();
+
+
+            int Add()
+            {
+                return x + y;
+            }
+        }
+
+        static int Add(int x, int y)
+        {
+            //Do some validation here
+            return x + y;
+        }
+    }
+}
